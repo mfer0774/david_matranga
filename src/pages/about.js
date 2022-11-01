63bec2dddcff1d80e42362aca4fd51a898c0fada
mfer0774 <mfer0774@gmail.com>
2022-11-01 10:23:23 -0500

    css things

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return(
+        <Layout pageTitle="About Me">
+            <p>About test texty text text</p>
+        </Layout>
+    )
+}
+
+export const Head = () => <title>David Matranga | About Me</title>
+
+export default AboutPage
